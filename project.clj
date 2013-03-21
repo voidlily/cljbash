4fcdba40fd725d303cf05558b1291bc25fbfe4c8
Lily <lily@voidlily.net>
2013-03-21 11:30:21 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject cljbash "0.1.0-SNAPSHOT"
+  :description "An IRC quote database"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler cljbash.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
